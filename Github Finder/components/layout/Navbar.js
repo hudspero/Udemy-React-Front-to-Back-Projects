7b6dd34bca17cd5abaa950082d791319402a7a76
Robert Hudspeth <hudspero@users.noreply.github.com>
2020-04-10 18:25:21 -0700

    Added updated files
    1) Added Alert.js to handle app-level alerts. This prevents returning bad error codes and results from the API.
    2) Added a spinner to play when paginated results are being retrieved.

--- Github Finder/components/layout/Navbar.js
+++ Github Finder/components/layout/Navbar.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
+/* Component for the bar at the top of the page */
 const Navbar = ({ icon, title }) => {
    return (
       <nav className='navbar bg-primary'>
          <h1>
+            {/* An example of implementing a prop defined in App.js, title*/}
             <i className={icon} /> {title}
-            {/*An example of implementing a prop defined in App.js, title */}
          </h1>
+         <ul>
+            <li>
+               {/*<Link to> is used in place of <a href> to preserve list of users returned from a search when clicking a link}*/}
+               <Link to='/'>Home</Link>
+            </li>
+            <li>
+               <Link to='/about'>About</Link>
+            </li>
+         </ul>
       </nav>
    );
 };
