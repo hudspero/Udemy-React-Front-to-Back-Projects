41c06cdeba0e123d21e39b29134aac9a68d4441f
Robert Hudspeth <hudspero@users.noreply.github.com>
2020-04-10 18:26:12 -0700

    Added updated files

--- Github Finder/components/users/UserItem.js
+++ Github Finder/components/users/UserItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+/* Component handling the formatting of each user returned from searching */
 const UserItem = ({ user: { login, avatar_url, html_url } }) => {
    return (
       <div className='card text-center'>
