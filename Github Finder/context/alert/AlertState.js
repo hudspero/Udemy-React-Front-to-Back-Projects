82654a39dcb437adf8130a88a790a317039c0ded
Robert Hudspeth <hudspero@users.noreply.github.com>
2020-04-25 21:19:57 -0700

    Added files for 3rd iteration
    -Refactored functions and programming away from App.js into dedicated files
    -Implemented Hooks, Contexts, and Reducers for the Github and Alert states, removing the need for props and prop-drilling between files
    -Added Home & User Not Found pages for completion

---
+++ Github Finder/context/alert/AlertState.js
@@ -0,0 +1,27 @@
+import React, { useReducer } from 'react';
+import AlertContext from './alertContext';
+import AlertReducer from './alertReducer';
+import { SET_ALERT, REMOVE_ALERT } from '../types';
+
+const AlertState = props => {
+   const InitialState = null;
+
+   const [state, dispatch] = useReducer(AlertReducer, InitialState);
+
+   /* When provided a message and a category, change the alert to active */
+   const setAlert = (msg, type) => {
+      dispatch({ type: SET_ALERT, payload: { msg, type } })
+      setTimeout(() => dispatch({ type: REMOVE_ALERT }), 3500)
+   }
+
+   return <AlertContext.Provider
+      value={{
+         alert: state,
+         setAlert
+      }}
+   >
+      {props.children}
+   </AlertContext.Provider>
+}
+
+export default AlertState;
